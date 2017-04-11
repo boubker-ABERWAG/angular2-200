dd40ea4a323da73a3ea5fd7a9a6bf527325885b8
Cyril Balit <cbalit@gmail.com>
2017-04-11 11:43:32 +0200

    clean step-02-solution

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { MaterialModule } from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { PeopleAppComponent } from './app.component';
+import { HomeComponent } from './home';
 
 @NgModule({
   imports: [
@@ -17,11 +18,12 @@ import { PeopleAppComponent } from './app.component';
     MaterialModule.forRoot()
   ],
   declarations: [
-    PeopleAppComponent
+    PeopleAppComponent,
+    HomeComponent
   ],
   providers: [ ],
   bootstrap: [
-    PeopleAppComponent
+    HomeComponent
   ]
 })
 export class AppModule { }
