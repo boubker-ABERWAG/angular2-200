7e2089917aee3cb3085125f9f68e192f352965e0
Cyril Balit <cbalit@gmail.com>
2017-04-11 12:41:22 +0200

    clean step-04-solution

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { PeopleAppComponent } from './app.component';
 import { HomeComponent } from './home';
+import { PersonComponent } from './person';
 
 @NgModule({
   imports: [
@@ -18,7 +19,8 @@ import { HomeComponent } from './home';
   ],
   declarations: [
     PeopleAppComponent,
-    HomeComponent
+    HomeComponent,
+    PersonComponent
   ],
   providers: [ ],
   bootstrap: [
