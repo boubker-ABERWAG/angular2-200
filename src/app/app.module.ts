f99d88019058fb3fb2eb2b862153447683ed615e
Cyril Balit <cbalit@gmail.com>
2017-04-11 11:30:52 +0200

    clean step-01

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,29 +3,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 // MATERIAL DESIGN MODULES
+import 'hammerjs';
 import { MaterialModule } from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 // import { APP_ROUTES } from './app.routes';
 
-import { PeopleAppComponent } from './app.component';
-import { HomeComponent } from './home/index';
-import { PersonComponent } from './person/index';
+
+//import { PeopleAppComponent } from './app.component';
 
 @NgModule({
   imports: [
     BrowserModule,
-    HttpModule,
-    MaterialModule.forRoot(),
-    // APP_ROUTES
+    BrowserAnimationsModule,
+    MaterialModule.forRoot()
   ],
   declarations: [
-    PeopleAppComponent,
-    HomeComponent,
-    PersonComponent
+    //PeopleAppComponent
   ],
   providers: [ ],
   bootstrap: [
-    PeopleAppComponent
+    //PeopleAppComponent
   ]
 })
 export class AppModule { }
