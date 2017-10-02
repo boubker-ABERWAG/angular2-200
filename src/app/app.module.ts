1859ff36fb74cc53dfb355717c8790615209069e
Cyril Balit <cbalit@gmail.com>
2017-10-02 10:13:39 +0200

    update dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 // MATERIAL DESIGN MODULES
-import 'hammerjs';
-import { MaterialModule } from '@angular/material';
+import {
+  MdToolbarModule,
+  MdCardModule,
+  MdTabsModule,
+  MdButtonModule,
+  MdInputModule,
+  MdCheckboxModule,
+  MdRadioModule,
+  MdIconModule,
+  MdListModule
+} from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 // import { APP_ROUTES } from './app.routes';
@@ -17,9 +26,15 @@ import { PersonComponent } from './person';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule.forRoot(),
-    HttpModule,
-    // APP_ROUTES
+    MdToolbarModule,
+    MdCardModule,
+    MdTabsModule,
+    MdButtonModule,
+    MdInputModule,
+    MdCheckboxModule,
+    MdRadioModule,
+    MdIconModule,
+    MdListModule
   ],
   declarations: [
     PeopleAppComponent,
