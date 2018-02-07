eebfe8fd48a4cd49191e897fc77200d321021b14
Fabien Zibi <fabien.zibi@gmail.com>
2018-02-07 11:00:01 +0100

    Missing injection

--- src/app/people/people.component.ts
+++ src/app/people/people.component.ts
@@ -18,7 +18,7 @@ export class PeopleComponent implements OnInit {
     dialogStatus = 'inactive';
     view = 'card';
 
-    constructor(private _http: HttpClient, public dialog: MatDialog) {}
+    constructor(private _http: HttpClient, public dialog: MatDialog, private _peopleService: PeopleService) {}
 
 
     /**
