9424d369b1270a34a047d62f6c9c0472fb304bc9
Fabien Zibi <fabien.zibi@gmail.com>
2018-02-07 10:50:50 +0100

    service path

--- src/app/person/person.component.ts
+++ src/app/person/person.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { PeopleService } from '../shared';
+import { PeopleService } from 'app/shared/people-service';
 
 @Component({
     selector: 'sfeir-person',
