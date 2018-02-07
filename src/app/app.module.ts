a03f5b408e908644752807be145143f7a7954d21
Fabien Zibi <fabien.zibi@gmail.com>
2018-02-07 18:20:09 +0100

    resolve branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,16 @@ import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 // MATERIAL DESIGN MODULES
 import {
-  MdToolbarModule,
-  MdCardModule,
-  MdTabsModule,
-  MdButtonModule,
-  MdInputModule,
-  MdCheckboxModule,
-  MdRadioModule,
-  MdIconModule,
-  MdListModule,
-  MdDialogModule
+  MatToolbarModule,
+  MatCardModule,
+  MatTabsModule,
+  MatButtonModule,
+  MatInputModule,
+  MatCheckboxModule,
+  MatRadioModule,
+  MatIconModule,
+  MatListModule,
+  MatDialogModule
 } from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
@@ -34,29 +34,25 @@ import { NaPipe } from './shared/na-pipe';
 import { SearchComponent } from './shared/search/search.component';
 import { SfeirBadgeDirective } from './shared/badge';
 import { reducers, metaReducers } from './store/reducers';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MdToolbarModule,
-    MdCardModule,
-    MdTabsModule,
-    MdButtonModule,
-    MdInputModule,
-    MdCheckboxModule,
-    MdRadioModule,
-    MdIconModule,
-    MdListModule,
-    MdDialogModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatTabsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatIconModule,
+    MatListModule,
+    MatDialogModule,
     HttpClientModule,
     APP_ROUTES,
     ReactiveFormsModule,
     StoreModule.forRoot(reducers, {metaReducers}),
-    StoreDevtoolsModule.instrument({
-      maxAge: 25
-    })
   ],
   declarations: [
     PeopleAppComponent,
