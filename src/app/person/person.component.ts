9c1b737c0d9ea03ebbd66fb980bc4db967406b62
baberwag <b.aberwag@gmail.com>
2018-03-02 11:38:53 +0100

    Delete unused images and format code

--- src/app/person/person.component.ts
+++ src/app/person/person.component.ts
@@ -1,28 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import { PEOPLE } from '../_static/people';
+import { PEOPLE } from './people';
 
 @Component({
     selector: 'sfeir-person',
     templateUrl: 'person.component.html',
     styleUrls: ['person.component.css']
 })
-export class PersonComponent implements OnInit {    
-    private person: any;
-    
-    constructor() {
-        this.person = PEOPLE[0];
-    }
-    
-    /**
-     * OnInit implementation
-     */
-    ngOnInit() {
-    }
+export class PersonComponent implements OnInit {
+  private person: any;
 
-    /**
-     * Returns random people
-     */
-    random() {
-        this.person = PEOPLE[ (Math.random() * PEOPLE.length) | 0 ];
-    }
+  constructor() {}
+
+  /**
+   * OnInit implementation
+   */
+  ngOnInit() {
+    this.person = PEOPLE[0];
+  }
+
+  /**
+   * Returns random people
+   */
+  random() {
+      this.person = PEOPLE[ (Math.random() * PEOPLE.length) | 0 ];
+  }
 }
